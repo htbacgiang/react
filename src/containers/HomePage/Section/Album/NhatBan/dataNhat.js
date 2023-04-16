38fc37592304cc0d3689155f04b482259f8e3239
htbacgiang <80782829+htbacgiang@users.noreply.github.com>
2023-04-16 21:55:11 +0900

    ht

--- src/containers/HomePage/Section/Album/NhatBan/dataNhat.js
+++ src/containers/HomePage/Section/Album/NhatBan/dataNhat.js
@@ -1,32 +1,26 @@
 export const photos = [
-	
-	  {
-		src: "",
+	{
+		src: "https://live.staticflickr.com/65535/52821509900_dc263f595c_b.jpg",
 		width: 6,
 		height: 4
 	  },
 	  {
-		src: "",
+		src: "https://live.staticflickr.com/65535/52821539258_3b28b03c08_b.jpg",
 		width: 6,
 		height: 4
 	  },
 	  {
-		src: "",
+		src: "https://live.staticflickr.com/65535/52821539228_fd359fa5b3_b.jpg",
+		width: 4,
+		height: 6
+	  },
+	  {
+		src: "https://live.staticflickr.com/65535/52821509650_ab7d000463.jpg",
 		width: 6,
 		height: 4
 	  },
 	  {
-		src: "",
-		width: 6,
-		height: 4
-	  },
-	  {
-		src: "",
-		width: 6,
-		height: 4
-	  },
-	  {
-		src: "",
+		src: "https://live.staticflickr.com/65535/52821098726_686f2efc03_b.jpg",
 		width: 6,
 		height: 4
 	  },
